0cb40f98e0006ed2e25f9e7028f32ed8d2a0c527
David <davi251911@gmail.com>
2024-10-28 22:09:09 -0500

    ajustes de vistas

--- app/src/main/java/com/ordersapp/components/Components.kt
+++ app/src/main/java/com/ordersapp/components/Components.kt
@@ -1,14 +1,26 @@
 package com.ordersapp.components
 
+
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -16,12 +28,58 @@ import androidx.compose.ui.unit.sp
 fun NormalTextComponents(value: String) {
     Text(
         text = value,
-        modifier = Modifier.fillMaxWidth().heightIn(min = 80.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 40.dp),
         style = TextStyle(
-            fontSize = 18.sp,
+            fontSize = 24.sp,
             fontWeight = FontWeight.Normal,
             fontStyle = FontStyle.Normal
-        )
+        ),
+        color = Color.Black,
+       textAlign = TextAlign.Center
     )
+}
 
+@Composable
+fun HeadingTextComponents(value: String) {
+    Text(
+        text = value,
+        modifier = Modifier.fillMaxWidth(),
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal
+        ),
+        color = Color.Black,
+        textAlign = TextAlign.Center
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditTextComponents(labelValue: String, painterResource: Painter) {
+    val textValue = remember {
+        mutableStateOf("")
+    }
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(4.dp))
+            .heightIn(min = 40.dp),
+        label = { Text(text = labelValue) },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Gray,
+            focusedLabelColor = Color.Gray,
+            cursorColor = Color.Black
+        ),
+        keyboardOptions = KeyboardOptions.Default,
+        value = textValue.value,
+        onValueChange = {
+            textValue.value = it
+        },
+        leadingIcon = {
+            Icon(painter = painterResource, contentDescription = "")
+        }
+    )
 }
--- app/src/main/java/com/ordersapp/screens/SignUpScreen.kt
+++ app/src/main/java/com/ordersapp/screens/SignUpScreen.kt
@@ -1,20 +1,35 @@
 package com.ordersapp.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ordersapp.R
+import com.ordersapp.components.EditTextComponents
+import com.ordersapp.components.HeadingTextComponents
 import com.ordersapp.components.NormalTextComponents
 
 @Composable
 fun SignUpScreen () {
     Surface (
         color = Color.White,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(28.dp)
     ) {
-        NormalTextComponents(value = "")
+        Column (modifier = Modifier.fillMaxSize()) {
+            NormalTextComponents(value = stringResource(id = R.string.Hey_there))
+            HeadingTextComponents(value = stringResource(id = R.string.welcome))
+            EditTextComponents(labelValue = stringResource(id = R.string.first_name))
+        }
     }
 }
 
